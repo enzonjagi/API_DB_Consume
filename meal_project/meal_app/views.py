e62979aba75b03369be595eba039306fc08b71a5
enzonjagi <amosndungo@gmail.com>
2023-05-17 17:00:18 +0300

    An application to display meals by consuming data from an api

---
+++ meal_project/meal_app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from .models import Meal
+import requests
+
+def get_meals(request):
+    """A view for the meals we receive from the API"""
+
+    all_meals = {}
+
+    if 'name' in request.GET:
+        name = request.GET['name']
+        url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=%s' % name
+        response = requests.get(url)
+        data = response.json()
+        meals = data['meals']
+
+        for meal in meals:
+            meal_data = Meal(
+                name = meal['strMeal'],
+                category = meal['strCategory'],
+                instructions = meal['strInstructions'],
+                region = meal['strArea'],
+                slug = meal['idMeal'],
+                image_url = meal['strMealThumb']
+            )
+            meal_data.save()
+            all_meals = Meal.objects.all().order_by('-id')
+
+    return render(request, 'meals/meal.html', {"all_meals": all_meals})
+
+def meal_detail(request):
+    """A view for individual meals"""
+    pass
